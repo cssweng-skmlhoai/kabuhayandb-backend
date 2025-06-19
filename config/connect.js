9f6addb16e06b2b74e8d51f8de737e4f4b4b74f0
Nate Tolentino <ntole24@gmail.com>
2025-06-19 19:58:07 +0800

    Connected the backend to the db (through the test.js file muna)

--- config/connect.js
+++ config/connect.js
@@ -1,10 +1,10 @@
 import mysql from 'mysql2/promise';
 
 const pool = mysql.createPool({ // FUTURE: put this all in a .env file for security purposes
-  host: 'insert host here',
-  user: 'insert user here',
-  password: 'insert password here',
-  database: 'insert db name here',
+  host: 'gateway01.ap-southeast-1.prod.aws.tidbcloud.com',
+  user: '3kFNHppaS7n6CoU.root',
+  password: 'nQsP65GDdD7yh1Hj',
+  database: 'kabuhayan_db',
   port: 4000,
   ssl: {
     rejectUnauthorized: true
--- config/test.js
+++ config/test.js
@@ -2,8 +2,18 @@ import pool from './connect.js';
 
 async function testConnection() {
   try {
-    const [rows] = await pool.query('SELECT NOW() AS current_time');
-    console.log('✅ Connected to TiDB! Current time:', rows[0].current_time);
+    const values = [
+    [1200.50, 850.75, 300.00],
+    [1300.00, 900.00, 350.00],
+    ];
+
+    await pool.query(
+      'INSERT INTO `dues` (`Meralco`, `Maynilad`, `Septic_Tank`) VALUES ?',
+      [values]
+    );
+
+    const [rows] = await pool.query('SELECT * FROM dues');
+    console.log('✅ Connected to TiDB! Selected data:', rows);
   } catch (err) {
     console.error('❌ Connection failed:', err.message);
   } finally {
