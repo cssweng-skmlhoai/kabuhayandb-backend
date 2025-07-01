8bf6b6ef9ad12f8a44fa87abf6e82a1e4e7d874b
Solenad <roe.dizon80@gmail.com>
2025-07-01 23:55:59 +0800

    fix: getMembersHome route order

--- routes/members.routes.js
+++ routes/members.routes.js
@@ -3,13 +3,14 @@ import { Router } from 'express';
 
 const router = Router();
 
+router.get('/home', MembersController.getMembersHome);
+
 // generic id routes
 router.get('/:id', MembersController.getMembersById);
 router.delete('/:id', MembersController.deleteMembers);
 router.put('/:id', MembersController.updateMembers);
 
 // catch all routes
-router.get('/home', MembersController.getMembersHome);
 router.post('/', MembersController.createMembers);
 router.get('/', MembersController.getMembers);
 
