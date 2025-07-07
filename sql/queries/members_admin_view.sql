8012b2910e7896ca18d1955f2bb3c9359b1128ae
Nate Tolentino <ntole24@gmail.com>
2025-07-07 18:00:16 +0800

    feat: created select all members query with admin view details

---
+++ sql/queries/members_admin_view.sql
@@ -0,0 +1,14 @@
+SELECT 
+    CONCAT(m.first_name, ' ', m.middle_name, ' ', m.last_name) AS full_name,
+    h.tct_no,
+    h.block_no,
+    h.lot_no,
+    f.head_position
+FROM
+    kabuhayan_db.members m
+JOIN
+    kabuhayan_db.families f ON m.family_id = f.id
+JOIN
+    kabuhayan_db.households h ON f.household_id = h.id
+ORDER BY 
+    m.id;
