afa9c033766c62163b98d930fd35185eb9f69be8
roe <166220458+Solenad@users.noreply.github.com>
2025-07-25 14:38:22 +0800

    Feat/certification services (#79)
    * feat: added certifications services
    
    * fix: fixes to cert services
    
    * feat: added crn generator
    
    * fix: dues report subquery issue
    
    * fix: subquery issue
    
    * feat: added verifications in password update, fixed tests for it
    
    * feat: added file handling for member routes

--- routes/members.routes.js
+++ routes/members.routes.js
@@ -1,24 +1,41 @@
 import * as MembersController from '../controllers/members.controllers.js';
 import { authenticateApiSecret } from '../middlewares/auth.middleware.js';
 import { Router } from 'express';
+import { upload } from '../middlewares/multer.middleware.js';
 
 const router = Router();
 
 router.use(authenticateApiSecret);
 
-router.get('/info/:id', MembersController.getMemberInfoById);
-router.put('/info/:id', MembersController.updateMemberInfo);
-router.get('/home', MembersController.getMembersHome);
-router.post('/info', MembersController.createMemberInfo);
+router.put(
+  '/info/:id',
+  upload.single('signature'),
+  MembersController.updateMemberInfo
+);
+router.post(
+  '/info',
+  upload.single('signature'),
+  MembersController.createMemberInfo
+);
 
 // generic id routes
+router.get('/info/:id', MembersController.getMemberInfoById);
+router.get('/home', MembersController.getMembersHome);
+
 router.get('/:id', MembersController.getMembersById);
 router.delete('/:id', MembersController.deleteMembers);
-router.put('/:id', MembersController.updateMembers);
-router.put('/:id', MembersController.updateMemberMultiple);
+router.patch(
+  '/:id',
+  upload.single('signature'),
+  MembersController.updateMembers
+);
+router.put(
+  '/:id',
+  upload.single('signature'),
+  MembersController.updateMemberMultiple
+);
 
 // catch all routes
-router.get('/home', MembersController.getMembersHome);
-router.post('/', MembersController.createMembers);
+router.post('/', upload.single('signature'), MembersController.createMembers);
 router.get('/', MembersController.getMembers);
 export default router;
